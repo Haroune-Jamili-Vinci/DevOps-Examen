643d2a5de1c95303f12cef9900c94c76b58fea14
Harounito <Haroune>
2025-01-17 17:40:59 +0100

    add : when  giving  special charactere return true

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -47,6 +47,47 @@ describe("passwordChecker function should return the answer", function () {
 
 
 
+    it("Should return true for 8 characters ", function () {
+        const main = new Main();
+
+        const result = main.passwordChecker("bacloamp");
+
+        expect(result).toBe(true);
+    })
+
+
+
+    it("Should return true for 8 characters ", function () {
+        const main = new Main();
+
+        const result = main.passwordChecker("bacloamp");
+
+        expect(result).toBe(true);
+    })
+
+
+    it("Should return false when not special character", function () {
+        const main = new Main();
+
+        const result = main.specialCharactereFunction("bacloampsqs");
+
+        expect(result).toBe(false);
+    })
+
+    it("Should return false when not special character", function () {
+        const main = new Main();
+
+        const result = main.specialCharactereFunction("bacloam_s@s");
+
+        expect(result).toBe(false);
+    })
+
+
+
+
+
+
+
 
 
 })
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,8 +1,18 @@
 export class Main {
     passwordChecker(password) {
+
         if (password.length < 8) return false
 
+
         return true;
+
+    }
+
+    specialCharactereFunction(password) {
+        const regex = /[!@#$%^&*(),.?":{}|<>]/;
+
+        return regex.test(password);
+
     }
 
 }
